ed1972a3ee3913c627050d977e79250ea70f4396
paullavigne2001-sudo <paullavigne2001@gmail.com>
2026-04-30 13:49:31 +0200

    Update update_data.py

--- update_data.py
+++ update_data.py
@@ -45,7 +45,7 @@ def get_fred(series, limit=None):
 
 
 euribor   = get_fred("IR3TIB01EZM156N")
-inflation = get_fred("CP0000EZ19M086NEST")
+inflation = get_fred("FPCPITOTLZGEMU")
 bce       = get_fred("ECBDFR", limit=365)
 oat       = get_fred("IRLTLT01FRM156N")
 
